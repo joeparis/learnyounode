8a70a37c14b7289cd33a3e11a2bc18a3c5f5256a
Joe Paris <joe.paris@outlook.com>
2016-11-02 23:15:25 -0700

    Finished filtered ls

---
+++ filtered-ls.js
@@ -0,0 +1,19 @@
+var fs   = require('fs');
+var path = require('path');
+
+var dir = process.argv[2];
+var ext = '.' + process.argv[3];
+
+
+fs.readdir(dir, (error, files) => {
+    if(error)
+        throw error;
+
+    files = files.filter( (value) => {
+        return path.extname(value) === ext;
+    });
+
+    files.forEach( (file) => {
+        console.log(file);
+    });
+});
